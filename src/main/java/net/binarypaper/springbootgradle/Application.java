a7265bfa7073793f9d245af2a5ab081585b543fa
William Gadney <gadnex@gmail.com>
2023-07-24 11:51:51 +0200

    Add actuator info endpoint and swagger info

--- src/main/java/net/binarypaper/springbootgradle/Application.java
+++ src/main/java/net/binarypaper/springbootgradle/Application.java
@@ -2,8 +2,30 @@ package net.binarypaper.springbootgradle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 
 @SpringBootApplication
+@PropertySource("classpath:META-INF/build-info.properties")
+@OpenAPIDefinition(
+	info = @Info(
+		title = "${build.name}", 
+		description = "${build.description}", 
+		version = "${build.version}",
+		contact = @Contact(
+			name = "${build.developer.name}", 
+			email = "${build.developer.email}"
+		), 
+		license = @License(
+			name = "${build.license.name}", 
+			url = "${build.license.url}"
+		)
+	)
+)
 public class Application {
 
 	public static void main(String[] args) {
--- src/main/java/net/binarypaper/springbootgradle/hello/HelloController.java
+++ src/main/java/net/binarypaper/springbootgradle/hello/HelloController.java
@@ -11,7 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping(path = "hello")
 @Slf4j
 public class HelloController {
-    
+
     @GetMapping(path = "{name}")
     public String sayHello(@PathVariable(name = "name") String name) {
         log.info("name: {}", name);
