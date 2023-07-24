f91873b1f9af5f3ee8f6f06e5b2f5649aade3f85
William Gadney <gadnex@gmail.com>
2023-07-24 09:36:14 +0200

    Initial commit

---
+++ src/main/java/net/binarypaper/springbootgradle/hello/HelloController.java
@@ -0,0 +1,20 @@
+package net.binarypaper.springbootgradle.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequestMapping(path = "hello")
+@Slf4j
+public class HelloController {
+    
+    @GetMapping(path = "{name}")
+    public String sayHello(@PathVariable(name = "name") String name) {
+        log.info("name: {}", name);
+        return "Hello " + name;
+    }
+}
